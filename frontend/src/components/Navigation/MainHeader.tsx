2a2615889cf8ead143487dfbea8d9dae4feec22d
patricia <59278560+Patriciapavia@users.noreply.github.com>
2024-02-07 09:57:57 +1100

    create main header

---
+++ frontend/src/components/Navigation/MainHeader.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import UpcomingSessions from '../Sessions/UpComingSessions.jsx';
+import Button from '../UI/Button.jsx';
+
+export default function MainHeader() {
+    const [upcomingSessionsVisible, setUpcomingSessionsVisible] = useState(false);
+
+    function showUpcomingSessions() {
+        setUpcomingSessionsVisible(true);
+    }
+
+    function hideUpcomingSessions() {
+        setUpcomingSessionsVisible(false);
+    }
+    const listStyle = 'no-underline pb-1 border-b-2 border-transparent hover:text-teal-300'
+
+    return (
+        <>
+            {upcomingSessionsVisible && (
+                <UpcomingSessions onClose={hideUpcomingSessions} />
+            )}
+            <header className='w-4/5 max-w-[80rem] mx-auto my-8 flex justify-between items-center hover:border-teal-300 active:border-teale-300 focus:border-teal-300 focus:outline-none'>
+                <h1 className='m-0 text-lg text-teal-500'>7 Days</h1>
+                <nav>
+                    <ul className='flex gap-6 items-center text-teal-500'>
+                        <li className={listStyle}>
+                            <NavLink to="/" className={({ isActive }) => isActive ? 'active' : ''} end>Our Mission</NavLink>
+                        </li >
+                        <li className={listStyle}>
+                            <NavLink to="/sessions" className={({ isActive }) => isActive ? 'active' : ''}>Explore what's available</NavLink>
+                        </li>
+                        <li >
+                            <Button onClick={showUpcomingSessions}>Upcoming Booking</Button>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    );
+}
